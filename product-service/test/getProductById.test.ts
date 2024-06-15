59df150239d35cece77e4baa9bceaf27639d338d
Memo <memosoft@ya.ru>
2024-06-15 16:55:55 +0300

    feat: tests

---
+++ product-service/test/getProductById.test.ts
@@ -0,0 +1,26 @@
+import { handler } from '../src/lambda/getProductsById'
+import mockedList from '../src/lambda/mockedList'
+
+describe('productById', () => {
+  it('should be exist and eual and 200 response', async () => {
+    const item = mockedList[0]
+    // APIGatewayProxyEvent
+    const result = await handler({
+      pathParameters: { id: item.id },
+    } as any)
+    const respItem = JSON.parse(result.body)
+    expect(result.statusCode).toBe(200)
+    expect(respItem).toStrictEqual(item)
+  })
+
+  // 404
+  it('should be not exist and 404 response', async () => {
+    // APIGatewayProxyEvent
+    const result = await handler({
+      pathParameters: { id: 'dfgdfg' },
+    } as any)
+    const respItem = JSON.parse(result.body)
+    expect(result.statusCode).toBe(404)
+    expect(respItem).toStrictEqual({ message: 'Product not found' })
+  })
+})
